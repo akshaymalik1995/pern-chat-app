83efcfbb9c11ff28acc483e4c45b3f73a4c43d01
akshaymalik1995 <akshaymalik191@gmail.com>
2024-06-16 16:34:08 +0530

    chore: Update App component to use max height for better screen utilization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,41 @@
-
-import Home from './pages/Home'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { useAuthContext } from './context/AuthContext'
-import './App.css'
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useAuthContext } from "./context/AuthContext";
+import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 
-import { ToastContainer } from 'react-toastify'
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  const { authUser, isLoading } = useAuthContext()
-  console.log(authUser)
+  const { authUser, isLoading } = useAuthContext();
+  console.log(authUser);
   if (isLoading) {
-    return null
+    return null;
   }
-
 
   return (
     <>
-      <div className="h-screen max-h-screen flex items-center justify-center ">
+      <div className="flex h-[calc(100dvh)] items-center justify-center">
         <ToastContainer />
         <Routes>
-          <Route path="/" element={authUser ? <Home /> : <Navigate to={"/login"} />} />
-          <Route path="/login" element={!authUser ? <Login /> : <Navigate to={"/"} />} />
-          <Route path="/signup" element={!authUser ? <SignUp /> : <Navigate to={"/"} />} />
+          <Route
+            path="/"
+            element={authUser ? <Home /> : <Navigate to={"/login"} />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <Login /> : <Navigate to={"/"} />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUp /> : <Navigate to={"/"} />}
+          />
         </Routes>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
